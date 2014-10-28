2d75d0cb1e01d8210e118882c62e7f7723fdd55b
vnty <258015834@qq.com>
2014-10-28 20:40:45 +0800

    battle

---
+++ GUIExample/guaji/server/lua/battle.lua
@@ -0,0 +1,31 @@
+function init(my, target)
+     
+end
+
+function init()
+    local my = {};
+	--??
+	my.attack = 10;
+	my.attackMax = 100;
+	--??
+	my.def = 5;
+	my.defMax = 50;
+	--??
+	my.mdef = 5;
+	my.defMax = 50;
+	--???
+	my.hp = 1000;
+	my.hpMaX = 10000;
+	--???
+	my.mp = 200;
+	my.mpMax = 200;
+	--??
+	my.duck = 10;
+	--??
+	my.crit = 10;
+	--??
+	my.ten = 10;
+	--??
+	my.hit = 10;
+end
+
